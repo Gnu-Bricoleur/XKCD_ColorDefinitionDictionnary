0d200c4e55cae676e5941707eaf05c50aea76658
Sylvain Jean-Roger Migaud <sylvain.migaud@epfl.ch>
2020-10-05 23:12:34 +0200

    V1

---
+++ GenerateDictionnary.py
@@ -0,0 +1,28 @@
+from colour import Color
+
+
+with open("XKCDColors.txt") as f:
+	colours = f.readlines()
+
+file = open("XKCD_colour_definition.py","w")
+file.write("XKCD_RGB_TO_COLOR_NAMES = {")
+
+for colour in colours[1:-1]:
+	print(colour.strip())
+	values = colour.split("\t")
+	colour_name = values[0].replace(" ", "_")
+	colour_HEX = values[1].strip()
+	c = Color(colour_HEX)
+	file.write("(" + str(c.red) +", "+ str(c.green) + ", " + str(c.blue) + "): ['XKCD_" + colour_name + "'],\n")
+print(colours[-1].strip())
+values = colours[-1].split("\t")
+colour_name = values[0].replace(" ", "_")
+colour_HEX = values[1].strip()
+c = Color(colour_HEX)
+file.write("(" + str(c.red) +", "+ str(c.green) + ", " + str(c.blue) + "): ['XKCD_" + colour_name + "']\n")
+file.write("}")
+file.close() 
+
+ 
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# XKCD_ColorDefinitionDictionnary
+# Generate a python dictionnary file from the xkcd list of colours
+
+The purpose of this script is to automate the generation of a python dictionnary file from the XKCD color definition (https://blog.xkcd.com/2010/05/03/color-survey-results/)
+Ultimatly I'd like to use this dictionnary to complete the list of defines colours in the Python colour module (https://pypi.org/project/colour/)
+Pull request pending.
